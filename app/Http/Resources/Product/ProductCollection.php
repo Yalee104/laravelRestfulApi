68c35a846134c9e66ab4dbba63c4d35f3f56a869
Yun-Lin Lee <yl.lee@dexatek.com>
2018-12-29 22:32:54 +0800

    Create a different resource transformation for collections

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -15,11 +15,14 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 class ProductCollection extends ResourceCollection
 {
     /* Aaron
-        NOTE:   Collection is a plural of resource in this case 'Product', however, since we named
-                our resource as 'ProductResource' we will have to explicitly override it as shown below.
+        NOTE1:  Collection is a plural of resource in this case 'Product', however, since we named
+                our resource as 'ProductResourceCustomForCollect' we will have to explicitly override it as shown below.
                 We dont need to override if we named our resource simply as 'Product.php'.
+        NOTE2:  You can see that we have 2 Product resources, one is called ProductResource while the other one is called
+                ProductResourceCustomForCollect, the later one is used for collection since we would like to
+                show the data from resource in a collection differently.
     */
-    public $collects = 'App\Http\Resources\Product\ProductResource'; 
+    public $collects = 'App\Http\Resources\Product\ProductResourceCustomForCollect';
 
     /**
      * Transform the resource collection into an array.
@@ -30,14 +33,13 @@ class ProductCollection extends ResourceCollection
     public function toArray($request)
     {
         /* Aaron
-            NOTE: The collection is from ProductResource.php of the transformed data!.
+            NOTE: The collection is from ProductResourceCustomForCollect.php of the transformed data!.
         */
         return [
             'data' => $this->collection,
-            'Other Meta' => 'Other Meta Data if needed'
+            //'Other Meta' => 'Other Meta Data if needed'
             //Note that pagination 'link' and 'meta' will automatically added in the response if the request contain pagination (see ProductCOntroller.php)
         ];
         
-        //return parent::toArray($request);
     }
 }
