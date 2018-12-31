c856ed2283b9aa84d76115fcb63781ca4637d3ec
Yun-Lin Lee <yl.lee@dexatek.com>
2018-12-31 18:08:22 +0800

    Handles Exception, but this is just an example to demonstrate the concept as its is not SOLID, in this case it violates Single responsibility principle.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,10 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +49,29 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        /*  Aaron
+         *  NOTE1:  Here we are adding exception handlings, below is just an example of checking if the request
+         *          is expect json (eg, Accept application/json), otherwise we will return web html exception.
+         *  NOTE2:  In order to find which instance of exception that it belongs to us dd($exception)
+         *          to get the instance of failure scenario (it can be shown from PostMan response 'preview' window)
+         */
+        //dd($exception); //uncomment to show detail debug
+        if ($request->expectsJson())
+        {
+            if ($exception instanceof ModelNotFoundException)
+            {
+                return response()->json([
+                        'error' => 'Product Model Not Found'
+                    ], Response::HTTP_NOT_FOUND);
+            }
+
+            if ($exception instanceof  NotFoundHttpException)
+            {
+                return response()->json([
+                    'error' => 'Incorrect Route'
+                ], Response::HTTP_NOT_FOUND);
+            }
+        }
         return parent::render($request, $exception);
     }
 }
