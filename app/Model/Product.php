d6997cc8a0ae4822fac0e59fb0f4a49590f8f892
Yun-Lin Lee <yl.lee@dexatek.com>
2019-01-03 23:04:53 +0800

    Adding the phpstorm ide-healper for laravel

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -4,6 +4,33 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Model\Product
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $detail
+ * @property int $price
+ * @property int $stock
+ * @property int $discount
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Model\Review[] $reviews
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product whereDetail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product whereDiscount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product whereStock($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Product whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
     /*  Aaron
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -4,6 +4,29 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Model\Review
+ *
+ * @property int $id
+ * @property int $product_id
+ * @property string $customer
+ * @property string|null $review
+ * @property int $star
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Model\Product $product
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review whereCustomer($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review whereReview($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review whereStar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Review whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Review extends Model
 {
     /*  Aaron
