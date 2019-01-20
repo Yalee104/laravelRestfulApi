e8d7a578d83c870be81644530a1fe1f0a2b184ae
Yun-Lin Lee <yl.lee@dexatek.com>
2019-01-20 10:33:23 +0800

    Multi-Authentication Part.2 - Add Admin migration, factory, and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             Creating fake data using the factory we have build (check ProductFactory.php & ReviewFactory.php)
             related method can be found in https://laravel.com/docs/5.7/database-testing 
         */
+        factory(App\Admin::class, 1)->create();
         factory(App\User::class, 5)->create();
         factory(App\Model\Product::class, 50)->create();
         factory(App\Model\Review::class, 300)->create();
