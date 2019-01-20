90dc891ca6bc2e9d0561af7218f1be3430df0b96
Yun-Lin Lee <yl.lee@dexatek.com>
2019-01-20 21:53:41 +0800

    Multi-Authentication Part.4 - Add admin new view and adjust the layout file to accomodate the differences for admin and user such as the route and hidding registration for admin (eg, admin will not be able to register in guest mode).

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{ route('admin.login') }}">Admin Login</a>
+
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
