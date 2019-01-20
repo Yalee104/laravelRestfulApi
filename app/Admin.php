30e78d48098fecbd1c0c5b06a750131e4c369c8f
Yun-Lin Lee <yl.lee@dexatek.com>
2019-01-20 14:52:01 +0800

    Multi-Authentication Part.2.1 - Fix Admin model as it need to extend Authenticatable class (and we also added the use of Notifiable in case we need it in the future).

--- app/Admin.php
+++ app/Admin.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
+    use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
