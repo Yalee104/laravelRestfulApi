c68bb7131556546ac139f72be11cf66eecb7cf52
Yun-Lin Lee <yl.lee@dexatek.com>
2019-01-20 21:42:13 +0800

    Multi-Authentication Part.3.1 - Add admin controllers

---
+++ app/Http/Controllers/AdminHomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class AdminHomeController extends Controller
+{
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin/login';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('AdminHome', ['admin' => '1']);
+    }
+}
